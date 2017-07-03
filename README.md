a8ef13366e149d84e11d6d338632b03ddbc71c28
Max Scher <maxobaxo@gmail.com>
2017-07-02 22:08:41 -0700

    add 2nd test/spec for multiple-word inputs

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 It can capitalize a one-word, lower-case input.
     * Example Input: "beowulf"
     * Example Output: "Beowulf"
+It can capitalize every word of a multi-word input.
+    * Example Input: "the little mermaid"
+    * Example Output: "The Little Mermaid"
--- tests/TitleCaseGeneratorTest.php
+++ tests/TitleCaseGeneratorTest.php
@@ -15,5 +15,18 @@
             //Assert
             $this->assertEquals("Beowulf", $result);
         }
+
+        function test_makeTitleCase_multipleWords()
+        {
+            //Arrange
+            $test_TitleCaseGenerator = new TitleCaseGenerator;
+            $input = "the little mermaid";
+
+            //Act
+            $result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+            //Assert
+            $this->assertEquals("The Little Mermaid", $result);
+        }
     }
 ?>
